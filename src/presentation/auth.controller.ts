6dde35b49966dc0354a837d1ec7e784071ff1d1c
A-nd-I <andimoggo@gmail.com>
2026-04-11 18:53:06 -0500

    refactor: remove all the logic from routes and implemented completely in controller

--- src/presentation/auth.controller.ts
+++ src/presentation/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-// import fs from "node:fs";
+import fs from "node:fs";
 
 export class AuthController {
   //const url_base = './data-tmp';
@@ -8,18 +8,20 @@ export class AuthController {
   }
 
   public saveUser = (req: Request, res: Response) => {
-    const body = req.body as { user_name?: string; password?: string };
+    const body = req.body as { password: string, user_name: string };
     console.log(body);
-    // const { user_name, password } = body;
-    // const url_base = './data-tmp';
+    const { password, user_name } = body;
+    const url_base = './data-tmp';
 
-    // if (!fs.existsSync(url_base)) {
-    //   fs.mkdir(url_base, () => {
-    //     fs.writeFileSync(url_base + "/myfile2.txt", `${user_name} with pass ${password} `);
-    //   });
-    // }
-    //res.json({ response: `${user_name} with pass ${password} ` });
-    res.json({ response: "ok in controller" });
+    if (!fs.existsSync(url_base)) {
+      fs.mkdir(url_base, () => {
+        fs.writeFileSync(url_base + "/myfile1.txt", `${user_name} with pass ${password} `);
+      });
+    }
+
+    fs.writeFileSync(url_base + "/myfile1.txt", `${user_name} with pass ${password} `);
+
+    res.json({ response: `${user_name} with pass ${password} ` });
     return true;
   };
 }
